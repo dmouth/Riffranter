d8a0665fe72ce902e28e85c92b16be700820edc9
Ray Parker <RayParkerBassPlayer@gmail.com>
2011-05-08 12:54:08 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem "mongrel"
+              
+gem "mysql"
+gem "meta_where"
+gem "jquery-rails"
+
+
+
+group :development, :test do
+  gem "ruby-debug"
+  gem "redgreen"
+  gem "ZenTest"
+  gem "autotest-rails"
+  gem 'factory_girl_rails'
+  gem "shoulda"                           
+  gem "webrat"  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiffRanter::Application.config.secret_token = '0c346dd3161135e249de1536277ef0ee6355405b332820c6590f66d5ff8adfb35fd8e840c67bc8477b0654967e4a2a67bab5a632058f1a30a287ed71a69d12e4'
