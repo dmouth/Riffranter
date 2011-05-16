1ab99903aa454950a05e50efc5d7a4ccf923d6da
Ray Parker <RayParkerBassPlayer@gmail.com>
2011-05-16 16:32:03 -0400

    Work on rant#index.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       @rants = Rant.order(:created_at.desc).limit(5)
+      @persona = current_user.followed_personas.order("rand()").first
     end
   end
 end
