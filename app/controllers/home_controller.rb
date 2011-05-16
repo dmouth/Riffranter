ac1a34dec5a1d377c5e201a5a00822be9b7628f6
Ray Parker <RayParkerBassPlayer@gmail.com>
2011-05-16 16:51:34 -0400

    Fixed a bug before it happened.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       @rants = Rant.order(:created_at.desc).limit(5)
-      @persona = current_user.followed_personas.order("rand()").first
+      @persona = current_user.followed_personas.order("rand()").first || Persona.first
     end
   end
 end
