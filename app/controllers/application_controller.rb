6443e77cbbdae7fb99a3b290b86450c0c783410b
Ray Parker <RayParkerBassPlayer@gmail.com>
2011-05-16 15:50:08 -0400

    General fussing about.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user
+  helper_method :current_user?
   helper_method :owner_or_admin?
   
   def is_admin?          
@@ -32,5 +33,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end                          
+  end                         
+  
+  def current_user?
+    !current_user.nil?
+  end 
 end
