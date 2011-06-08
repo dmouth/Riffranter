68a59bfaef90566ed75ac20451886ff92720f5c8
Karl Parker <karl.parker@gmail.com>
2011-06-07 19:30:31 -0500

    Changed User Index

--- app/helpers/rants_helper.rb
+++ app/helpers/rants_helper.rb
@@ -1,2 +1,8 @@
 module RantsHelper
+  
+  def linkify rantText
+    
+    regex = Regexp.new '((https?:\/\/|www\.)([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)'
+    rantText.gsub!( regex, '<a href="\1">\1</a>' ) 
+  end
 end
