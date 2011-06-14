64832c9720627310f888c5261c578d509a13b23c
Ray Parker <RayParkerBassPlayer@gmail.com>
2011-06-14 00:27:13 -0400

    Fucking SQL syntax crap.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,7 +12,7 @@ class HomeController < ApplicationController
       @subscribed_rants = current_user.followed_rants
 
       # I'd definitely love to not do it this way, but my Arel chops aren't together enough to pull this one off.
-      @hot_rants = Rant.find_by_sql("SELECT rants.*, avg(votes.stars) as rating FROM rants INNER JOIN votes ON votes.rant_id = rants.id group by rants.id order by rating desc limit 5 ")
+      @hot_rants = Rant.find_by_sql("SELECT rants.id, rants.user_id, rants.persona_id, rants.body, rants.ip, rants.created_at, rants.updated_at, avg(votes.stars) as rating FROM rants INNER JOIN votes ON votes.rant_id = rants.id group by rants.id, rants.user_id, rants.persona_id, rants.body, rants.ip, rants.created_at, rants.updated_at order by rating desc limit 5")
     end
   end
 end
