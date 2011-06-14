7ecb60e36bfc1262641f854bc3f713c13be8d4d2
Ray Parker <RayParkerBassPlayer@gmail.com>
2011-06-14 00:09:47 -0400

    Fixed random difference between prod and dev.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,9 @@ class HomeController < ApplicationController
       # Newest rants
       @rants = Rant.order(:created_at.desc).includes(:user, :persona, :votes).limit(5)
 
-      # Randomly select a persona for the rant widget
-      @persona = current_user.followed_personas.order("rand()").first || Persona.first
+      # Randomly select a persona for the rant widget  -- mysql local and postgre prod for now 
+      order = %Q[test development].include?(Rails.env) ? "rand()" : "random()"
+      @persona = current_user.followed_personas.order(order).first || Persona.first
 
       # All rants for personas and users that the current user is following
       @subscribed_rants = current_user.followed_rants
