ab980eef79345b26f484577c6abdfc95b44cd3ab
Ray Parker <RayParkerBassPlayer@gmail.com>
2011-07-01 08:52:29 -0400

    Removed title validation.

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -2,16 +2,15 @@ class Persona < ActiveRecord::Base
   has_friendly_id :name, :use_slug => true, :approximate_ascii => true, :max_length => 50
 
   validates :name, :presence => true, :uniqueness => true
-  validates :title, :presence => true
-  
+
   has_many :rants, :dependent => :destroy
   has_and_belongs_to_many :following_users, :class_name => "User", :join_table => :personas_users
   has_and_belongs_to_many :categories, :order => :name
 
   mount_uploader :image, ImageUploader
-  
+
   scope :for_category, lambda{|c| joins(:categories).select("distinct personas.*").where("categories.id = #{c.id}")}
-  
+
   def following_users_for_mail
     following_users.where(:send_mail_updates >> true)
   end
